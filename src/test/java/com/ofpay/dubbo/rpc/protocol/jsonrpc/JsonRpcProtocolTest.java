0c2bec016eb395c62c8272579c4a8c003c0cd4da
cuikexiang <cuikexiang@quancheng-ec.com>
2016-02-14 17:00:14 +0800

    升级jetty到9
    update doc
    
    升级jetty到jetty9.跟新配置文档
    
    与原作者版本号一致，还原老版本jetty支持，还原老版本jetty测试用例。push

--- src/test/java/com/ofpay/dubbo/rpc/protocol/jsonrpc/JsonRpcProtocolTest.java
+++ src/test/java/com/ofpay/dubbo/rpc/protocol/jsonrpc/JsonRpcProtocolTest.java
@@ -1,5 +1,9 @@
 package com.ofpay.dubbo.rpc.protocol.jsonrpc;
 
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.rpc.Exporter;
@@ -7,12 +11,9 @@ import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Protocol;
 import com.alibaba.dubbo.rpc.ProxyFactory;
 import com.ofpay.dubbo.rpc.protocol.jsonrpc.JsonRpcServiceImpl.MyException;
-import org.junit.Assert;
-import org.junit.Test;
 
 /**
- * JsonRpcProtocolTest
- * Created by wuwen on 15/4/1.
+ * JsonRpcProtocolTest Created by wuwen on 15/4/1.
  */
 public class JsonRpcProtocolTest {
 
@@ -33,14 +34,33 @@ public class JsonRpcProtocolTest {
         exporter.unexport();
     }
 
-    /**
-     *TODO 暂不支持自定义异常
     @Test
+    public void testJsonrpcProtocolForServerJetty9() {
+        JsonRpcServiceImpl server = new JsonRpcServiceImpl();
+        Assert.assertFalse(server.isCalled());
+        ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
+        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
+        URL url = URL.valueOf("jsonrpc://127.0.0.1:5342/" + JsonRpcService.class.getName() + "?version=1.0.0&server=jetty9");
+        Exporter<JsonRpcService> exporter = protocol.export(proxyFactory.getInvoker(server, JsonRpcService.class, url));
+        Invoker<JsonRpcService> invoker = protocol.refer(JsonRpcService.class, url);
+        JsonRpcService client = proxyFactory.getProxy(invoker);
+        String result = client.sayHello("haha");
+        Assert.assertTrue(server.isCalled());
+        Assert.assertEquals("Hello, haha", result);
+        invoker.destroy();
+        exporter.unexport();
+    }
+
+    // TODO 暂不支持自定义异常
+
+    @Test
+    @Ignore
     public void testCustomException() {
         JsonRpcServiceImpl server = new JsonRpcServiceImpl();
         ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
         Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
-        URL url = URL.valueOf("jsonrpc://127.0.0.1:5342/" + JsonRpcService.class.getName() + "?version=1.0.0");
+        URL url = URL.valueOf("jsonrpc://127.0.0.1:5342/" +
+                JsonRpcService.class.getName() + "?version=1.0.0&server=jetty9");
         Exporter<JsonRpcService> exporter = protocol.export(proxyFactory.getInvoker(server, JsonRpcService.class, url));
         Invoker<JsonRpcService> invoker = protocol.refer(JsonRpcService.class, url);
         JsonRpcService client = proxyFactory.getProxy(invoker);
@@ -51,6 +71,6 @@ public class JsonRpcProtocolTest {
         }
         invoker.destroy();
         exporter.unexport();
-    }***/
+    }
 
 }
